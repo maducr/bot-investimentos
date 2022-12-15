3eec4f5e901143cbecf4af2ab7ced3e2c925c7d0
Maria Eduarda Rodrigues <crmariaeduarda2003@gmail.com>
2022-12-14 20:21:42 -0400

    server.js

--- bot/server.js
+++ bot/server.js
@@ -6,7 +6,23 @@ const axios = require('axios')
 const bot = new Telegraf(env.token)
 
 const tecladoOpcoes = Markup.keyboard([
-    ['O que o bot faz?', 'Consultar ações!']
+    ['O que o bot faz?', '/menu'],
+]).resize().extra()
+
+const tecladoOpcoesIniciais = Markup.keyboard([
+    ['Ações']
+]).resize().extra()
+
+const tecladoAcoes = Markup.keyboard([
+    ['Consultar ações!']
+]).resize().extra()
+
+const tecladoOpcoes2 = Markup.keyboard([
+    ['Consultar outra ação!', 'Não, obrigado!']
+]).resize().extra()
+
+const tecladoReiniciar = Markup.keyboard([
+    ['/start']
 ]).resize().extra()
 
 bot.start(async ctx => {
@@ -16,31 +32,61 @@ bot.start(async ctx => {
 })
 
 bot.hears('O que o bot faz?', ctx => {
-    ctx.replyWithMarkdown('Esse Bot irá te ajudar a conferir e companhar as ações da bolsa de valores!', tecladoOpcoes)
+    ctx.replyWithMarkdown('Esse Bot irá te ajudar a conferir e companhar as ações da bolsa de valores!' + '\nVamos conferir?', tecladoOpcoes)
+})
+
+bot.hears('/menu', ctx => {
+    ctx.replyWithMarkdown('Escolha uma opção: ', tecladoOpcoesIniciais)
+})
+
+bot.hears('Ações', ctx => {
+    ctx.replyWithMarkdown('Vamos começar!', tecladoAcoes)
 })
 
 bot.hears('Consultar ações!', ctx => {
     ctx.replyWithMarkdown('Qual ação deseja consultar?');
     bot.on("text", async (ctx) => {
-        await ctx.telegram
-            .sendMessage(ctx.message.chat.id, ctx.message.chat)
-            .then(() => {
-                var url_da_api = "https://brapi.dev/api/quote/";
-                var acao = ctx.update.message.text;
-                
-                fetch((url_da_api + acao),{
-                         method: 'GET',
-                         headers: {
-                                 'Accept': 'application/json',
-                             }
-                      })
-                     .then(resposta_da_api => resposta_da_api.json())
-                     .then(resposta_da_api => {
-                        ctx.reply("As ações da " + acao + " estao custando " + resposta_da_api.results[0].regularMarketPrice + " no momento.");
-                     });
-             });
+        var url_da_api = "https://brapi.dev/api/quote/";
+        var acao = ctx.update.message.text;
+        
+        fetch((url_da_api + acao),{
+                    method: 'GET',
+                    headers: {
+                            'Accept': 'application/json',
+                        }
+                })
+                .then(resposta_da_api => resposta_da_api.json())
+                .then(resposta_da_api => {
+                    ctx.reply( `${resposta_da_api.results[0].symbol} - ${resposta_da_api.results[0].longName}` + `\n-> Cotação: ${resposta_da_api.results[0].regularMarketPrice}` + `\n-> Mínima do dia: ${resposta_da_api.results[0].regularMarketDayLow}⬆️` + `\n-> Máxima do dia: ${resposta_da_api.results[0].regularMarketDayHigh}⬇️` + `\n-> Mínima 52 semanas: ${resposta_da_api.results[0].fiftyTwoWeekLow}📉` + `\n-> Máxima 52 semanas: ${resposta_da_api.results[0].fiftyTwoWeekHigh}📈`);
+                })
+                .then( () => { ctx.replyWithMarkdown(`Posso te ajudar em algo mais?`, tecladoOpcoes2) });
    });
 })
 
 
+bot.hears('Consultar outra ação!', ctx => {
+    ctx.replyWithMarkdown('Qual ação deseja consultar?');
+    bot.on("text", async (ctx) => {
+        var url_da_api = "https://brapi.dev/api/quote/";
+        var acao = ctx.update.message.text;
+        
+        fetch((url_da_api + acao),{
+                    method: 'GET',
+                    headers: {
+                            'Accept': 'application/json',
+                        }
+                })
+                .then(resposta_da_api => resposta_da_api.json())
+                .then(resposta_da_api => {
+                    ctx.reply( `${resposta_da_api.results[0].symbol} - ${resposta_da_api.results[0].longName}` + `\nCusto atual: ${resposta_da_api.results[0].regularMarketPrice}` + `\nMenor custo: ${resposta_da_api.results[0].regularMarketDayLow}` + `\nMaior curso: ${resposta_da_api.results[0].regularMarketDayHigh}` + `\nPreço de fechamento: ${resposta_da_api.results[0].regularMarketPreviousClose}` + `\nPreço de abertura de pregão: ${resposta_da_api.results[0].regularMarketOpen}`);
+                });
+   });
+})
+
+bot.hears('Não, obrigado!', ctx => {
+    ctx.replyWithMarkdown('Obrigada por usar nosso bot.' + '\nVolte sempre!😊', tecladoReiniciar)
+})
+
+
 bot.startPolling()
+
